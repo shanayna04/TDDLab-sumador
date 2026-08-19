d3c474ef7f203f912db047ebbb30c63ba693410c
Shanayna De Ugarte <shany.de.ugarte.q@gmail.com>
2026-08-19 18:36:56 -0400

    feat: agrega funcionalidad de multiplicar

--- src/presenter.js
+++ src/presenter.js
@@ -1,9 +1,11 @@
 import sumar from "./sumador";
+import multiplicar from "./multiplicador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
+const multiplicarButton = document.querySelector("#multiplicar-button");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -12,4 +14,11 @@ form.addEventListener("submit", (event) => {
   const secondNumber = Number.parseInt(second.value);
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+multiplicarButton.addEventListener("click", () => {
+  const firstNumber = Number.parseInt(first.value);
+  const secondNumber = Number.parseInt(second.value);
+
+  div.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
 });
